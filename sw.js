0039588a0d1a3e337dcaa53e5b2d41e806d9ab1e
saad-coder-pro <msaadhyd.15@gmail.com>
2025-08-24 09:08:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -29,14 +29,30 @@ const ASSETS = [
   "./sfx/start.wav"
 ];
 
+
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting(); // Ensures SW activates immediately after install
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim(); // Take control of open clients immediately
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
